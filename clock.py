1eb541e59f8353329a06119c04168d8808769b12
Richa-bharti <richabharti906@gmail.com>
2020-10-01 18:37:53 +0530

    small changes

--- clock.py
+++ clock.py
@@ -6,10 +6,10 @@ import time
 def clock1():
 	time_str=time.strftime("%H:%M:%S")
 	clock.config(text=time_str)
-	clock.after(200,clock1)
+	clock.after(190,clock1)
 
 root=Tk()
-clock= Label(root,font={"times", 200, "bold"}, bg="white",fg="red")
+clock= Label(root,font={"times", 200, "bold"}, bg="blue",fg="green")
 clock.pack(fill=BOTH, expand=1)
 clock1()
 root.mainloop()
